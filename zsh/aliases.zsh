16f44ee677e7617b07cda986abfdc8d3239553fa
amihay <amihay@brodmann17.com>
2020-01-19 10:58:06 +0200

    scripts

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,20 @@
 alias reload!='. ~/.zshrc'
 
+
+alias gssh='gcloud compute ssh '
 alias cls='clear' # Good 'ol Clear Screen command
+
+alias k='kubectl ' # for kube
+
+alias venv3="source ~/.venv3/bin/activate"
+alias venv2="source .venv2/bin/activate"
+alias venv="source .venv/bin/activate"
+
+alias tailf="tail -f "
+
+alias k='kubectl'
+
+
+mount_brodmann33() {
+	sshfs brodmann-33:$1 /Users/amgonen/media/brodmann-33/$1 -o allow_other -o ro
+}
--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,5 +1,6 @@
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export CLICOLOR=true
+export LC_ALL=en_US.UTF-8
 
 fpath=($ZSH/functions $fpath)
 
@@ -37,3 +38,134 @@ bindkey '^[[5D' beginning-of-line
 bindkey '^[[5C' end-of-line
 bindkey '^[[3~' delete-char
 bindkey '^?' backward-delete-char
+
+source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# Path to your oh-my-zsh installation.
+export ZSH="/Users/amgonen/.oh-my-zsh"
+source ~/.zplug/init.zsh
+
+# Set name of the theme to load --- if set to "random", it will
+# load a random theme each time oh-my-zsh is loaded, in which case,
+# to know which specific one was loaded, run: echo $RANDOM_THEME
+# See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
+#ZSH_THEME="spaceship"
+#ZSH_THEME="powerlevel9k/powerlevel9k"
+ZSH_THEME="powerlevel10k/powerlevel10k"
+POWERLEVEL9K_MODE="awesome-patched"
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir newline  vcs)
+POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX="↱"
+POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="↳ "
+POWERLEVEL9K_PROMPT_ADD_NEWLINE=true
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status virtualenv root_indicator background_jobs time)
+POWERLEVEL9K_PROMPT_ON_NEWLINE=true
+POWERLEVEL9K_LEFT_SEGMENT_SEPARATOR=$'\uE0B1'
+POWERLEVEL9K_RIGHT_SEGMENT_SEPARATOR=$'\uE0B3'
+# Add a space in the first prompt
+POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX="%f"
+
+# Visual customisation of the second prompt line
+local user_symbol="$"
+if [[ $(print -P "%#") =~ "#" ]]; then
+user_symbol = "#"
+fi
+POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="%{%B%F{black}%K{yellow}%} $user_symbol%{%b%f%k%F{yellow}%} %{%f%}"
+POWERLEVEL9K_VCS_MODIFIED_BACKGROUND=’red’
+# Colorise the top Tabs of Iterm2 with the same color as background
+# Just change the 18/26/33 wich are the rgb values
+echo -e "\033]6;1;bg;red;brightness;18\a"
+echo -e "\033]6;1;bg;green;brightness;26\a"
+echo -e "\033]6;1;bg;blue;brightness;33\a"
+
+
+# Which plugins would you like to load?
+# Standard plugins can be found in ~/.oh-my-zsh/plugins/*
+# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+plugins=(
+git
+gitfast
+zsh-autosuggestions
+docker
+pip
+wd
+zsh-completions
+virtualenv
+docker-compose
+httpie
+dircycle
+vi-mode
+history 
+kubectl
+gitignore
+osx
+iterm2
+sublime
+pylint
+brew
+ssh-agent
+)
+
+
+setopt  autocd autopushd  pushdignoredups
+autoload -U compinit
+compinit
+
+d='dirs -v | head -10'
+
+source $ZSH/oh-my-zsh.sh
+zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
+# Do menu-driven completion.
+zstyle ':completion:*' menu select
+
+# Color completion for some things.
+# http://linuxshellaccount.blogspot.com/2008/12/color-completion-using-zsh-modules-on.html
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+
+# formatting and messages
+# http://www.masterzen.fr/2009/04/19/in-love-with-zsh-part-one/
+zstyle ':completion:*' verbose yes
+zstyle ':completion:*:descriptions' format "$fg[yellow]%B--- %d%b"
+zstyle ':completion:*:messages' format '%d'
+zstyle ':completion:*:warnings' format "$fg[red]No matches for:$reset_color %d"
+zstyle ':completion:*:corrections' format '%B%d (errors: %e)%b'
+zstyle ':completion:*' group-name ''
+
+
+fpath=(/usr/local/share/zsh-completions $fpath)
+source /usr/local/opt/git-extras/share/git-extras/git-extras-completion.zsh
+
+export EDITOR=/usr/bin/vim
+export VISUAL=/usr/bin/vim
+
+
+alias docker-run='docker run --rm -v $(pwd):/app frontender:latest "$@"'
+
+
+export TF_PLUGIN_CACHE_DIR="$HOME/.terraform.d/plugin-cache"
+mkdir -p $TF_PLUGIN_CACHE_DIR
+
+
+
+function what_is_my_ip() {
+public_ip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+private_ip=`/sbin/ifconfig utun1 | grep netmask | awk '{print $2}'| cut -f2 -d:`
+echo "My WAN/Public IP address: ${public_ip}, private_ip: ${private_ip}"
+}
+
+
+source ~/.iterm2_shell_integration.zsh
+
+export GOPATH=~/go
+export PATH=${PATH}:${GOPATH}/bin
+export JAVA_HOME=$(/usr/libexec/java_home)
+
+
+
+# Golang
+alias go_lint="golint ./... | grep -v comment | grep -v annoying | grep -v underscore"
+alias go_cov="go test ./... -coverprofile cp.out && go tool cover -html=cp.out"
+
+# to enable parallel running see https://stackoverflow.com/questions/50168647/multiprocessing-causes-python-to-crash-and-gives-an-error-may-have-been-in-progr
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
