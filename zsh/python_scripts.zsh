f23b620ff63f4daca63e6ea345b15f1fd4e9604a
Amihay Gonen <amihay@brodmann17.com>
2020-03-12 10:37:21 +0200

    git

--- zsh/python_scripts.zsh
+++ zsh/python_scripts.zsh
@@ -2,4 +2,6 @@ alias venv3="source ~/.venv3/bin/activate"
 alias venv2="source .venv2/bin/activate"
 alias venv="source .venv/bin/activate"
 alias venv_chraron="source /Users/amgonen/git/medulla-charon/.venv/bin/activate"
+alias venv_legcay="source /Users/amgonen/git/medulla-legcay/.venv/bin/activate"
+
 
