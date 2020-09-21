2c2781cc1d28944d048da0ab6cbd832150092493
Amihay Gonen <amihay@brodmann17.com>
2020-09-21 05:50:27 +0300

    update

--- zsh/datalake.zsh
+++ zsh/datalake.zsh
@@ -1,4 +1,11 @@
 #gls gs://brodmann_internal/datasources/datasource=fleet/stage=raw/datatype=video/setup=ffa757249bc7e679844c1408b3a01a44/video=ffa757249bc7e679844c1408b3a01a44/clip=na/
+gls_fleet_paths(){
+	echo video    : gs://brodmann_internal/datasources/datasource=fleet/stage=raw/datatype=video/setup=${1}/video=${1}/clip=na/\*
+	echo car_info : gs://brodmann_internal/datasources/datasource=fleet/stage=raw/datatype=fleet_capture_info/setup=${1}/video=${1}/clip=na/\*
+	echo stat : gs://brodmann_internal/datasources/datasource=fleet/stage=raw/datatype=fleet_frames_stat/setup=${1}/video=${1}/clip=na/\*
+
+}
+
 gls_fleet_carinfo() {
 	gsutil cat  gs://brodmann_internal/datasources/datasource=fleet/stage=raw/datatype=fleet_capture_info/setup=${1}/video=${1}/clip=na/\*
 }
