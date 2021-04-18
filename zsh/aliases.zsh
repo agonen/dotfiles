f55a0df672ab1ff2308db2d2a83ec37cbc40099f
Amihay Gonen <amihayg@elementor.com>
2021-04-18 12:47:20 +0300

    update dotfiles

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -15,4 +15,4 @@ alias tabview='vd'
 alias redshift_prod_amihay="pgcli -h redshift-elementor-prod.cwn3tgmpcufx.us-east-1.redshift.amazonaws.com -u amihayg -d dwh -p 5439"
 alias redshift_prod_su="pgcli -h redshift-elementor-prod.cwn3tgmpcufx.us-east-1.redshift.amazonaws.com -u awsuser -d dwh -p 5439"
 
-alias pg_db="pgcli -h localhost -u dbtuser  -d dbtdb  -p 5432"
+alias pg_db="pgcli -h localhost -u dbtuser  -d dwh  -p 5432"
