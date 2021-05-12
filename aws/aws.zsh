1b92804173f91e62e42a966a1162303280565fdc
Amihay Gonen <amihayg@elementor.com>
2021-05-12 12:29:03 +0300

    s3 utils

--- aws/aws.zsh
+++ aws/aws.zsh
@@ -15,6 +15,10 @@ ecs_tracker(){
     aws ecs run-task --capacity-provider-strategy capacityProvider=FARGATE_SPOT,weight=1 --cluster data --task-definition tracker --count 1 --network-configuration "awsvpcConfiguration={subnets=[subnet-d4a2dcfa,subnet-9f493af8],securityGroups=[sg-3cb1e57b],assignPublicIp=ENABLED}" --region us-east-1
 }
 
+s3cat(){
+    aws s3 cp  $1 - 
+}
+
 alias ecs_ps="ecs-cli ps"
 
 alias pip_refresh_token="aws codeartifact login --tool pip --repository data --domain elementor-data --domain-owner 061538166779"
